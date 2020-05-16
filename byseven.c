ed3742d4f1a214a98cf60bb7a67b0c5a978868b2
Yi Chen <yc311@duke.edu>
2020-05-16 00:17:02 -0400

    Now reads arg correctly as int

--- byseven.c
+++ byseven.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){ 
-	printf("%s", *argv);
-	return 0;
+int main(int argc, char* argv[]){
+
+	int input = atoi(argv[1]);
+ 
+	printf("The argument is %d\n", input);
+	return EXIT_SUCCESS;
 }
