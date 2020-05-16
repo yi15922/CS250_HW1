a86b23d703df035be971dbb4e8db97a231c2f7b0
Yi Chen <yc311@duke.edu>
2020-05-16 01:36:10 -0400

    Finalized recurse

--- recurse.c
+++ recurse.c
@@ -5,18 +5,13 @@ int recurse(int input){
 	if (input == 0){ 
 		return 2;
 	}
-	int result = 3*(input-1)+recurse(input-1)+1;
-	//printf("%d\n", result); 
-
-	return result;
+	return 3*(input-1)+recurse(input-1)+1;
 }
-
 
 int main (int argc, char* argv[]){ 
 	int input = atoi(argv[1]);	
-	int result = recurse(input); 
 
-	printf("%d\n", result); 
+	printf("%d\n", recurse(input)); 
 
 	return EXIT_SUCCESS;
 }
