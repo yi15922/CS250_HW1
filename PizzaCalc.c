71b85434bbe997152e6017a52d4dd6b9a0df5ba5
Yi Chen <yc311@duke.edu>
2020-05-20 20:21:18 -0400

    added comments and fixed styling

--- PizzaCalc.c
+++ PizzaCalc.c
@@ -20,46 +20,61 @@ typedef struct Pizza {
 } Pizza;
 
 
-
+/*
+ Function to create a Pizza node
+ takes FILE* as input
+ uses fgets() to read each line
+ uses a for loop to count tokens
+ allocates new Pizza* on each call
+ 
+ returns a Pizza*
+ */
 Pizza* create(FILE* theFile){
-    char str[MAX] = "empty";
+    char str[MAX] = "empty"; // Buffer, placeholder to check if file is empty
     char done[] = "DONE\n"; // Done has a newline after it... wtf?
 
+    // Allocate a new Pizza*
     Pizza* aPizza = (struct Pizza*)malloc(sizeof(Pizza));
 
+    // Token counter: 0 = name, 1 = diameter, 2 = cost
     for (int i = 0; i < 3; i++){
         
+        // Read one line
         fgets(str, MAX, theFile);
+        
+        // Buffer is unchanged if file is empty
         if (strcmp(str, "empty") == 0){
             printf("PIZZA FILE IS EMPTY");
             
             // If no pizza, panic and yeet out everything
 
             free(aPizza);
-            
             return NULL;
         }
         
+        // Return NULL if "DONE" is encountered
         if (strcmp(str, done) == 0){
             free(aPizza);
             return NULL;
         }
         
-        
+        // Make a copy of buffer
         char* copiedString = (char*)malloc(sizeof(char)*MAX);
         strcpy(copiedString, str);
         strtok(copiedString, "\n"); // Remove any newline character at the end
 
-        
+        // Token 1 = name
         if (i == 0){
             aPizza->name = copiedString;
         }
         
+        // Token 2 = diameter, temporarily storing in pizzaPerDollar
         if (i == 1){
             aPizza->pizzaPerDollar = atof(copiedString);
             free(copiedString);
         }
         
+        // Token 2 = cost, do the math
         if (i == 2){
             // If cost is 0, no pizza, there is no free lunch!
             if (!atof(copiedString)){
@@ -75,39 +90,56 @@ Pizza* create(FILE* theFile){
         }
     }
     
-    aPizza->next = NULL;
+    aPizza->next = NULL; // Initialize the next field
     return aPizza;
 }
 
+
+/*
+ Function for inserting nodes alphabetically
+ maybe a tree could work too?
+ 
+ Takes the pointer to the node being inserted and
+ the pointer to the head pointer of linked list
+ */
 void insert(Pizza* current, Pizza** head){
     
-    
+    // Declare a prev placeholder and an iterator
     Pizza* prev = NULL;
     Pizza* iter = *head;
     
+    // Advance to the element with a lower pizzaPerDOllar value
     while (iter != NULL && iter->pizzaPerDollar > current->pizzaPerDollar){
         prev = iter;
         iter = iter->next;
     }
     
-    
+    // If this is the first node
     if (prev == NULL){
         
-        current->next = iter;
-        *head = current;
+        current->next = iter; // Add the node to the beginning
+        *head = current; // And update the head pointer's pointer
 
     } else {
         
+        // In case the two pizzaPerDollar are the same
         while (iter != NULL && iter->pizzaPerDollar == current->pizzaPerDollar){
+            
+            // If the next node is higher alphabetically
             if (strcmp(current->name, iter->name) < 0){
+                
+                // Insert this node right before the next
                 prev->next = current;
                 current->next = iter;
                 return;
             }
+            
+            // Otherwise keep searching for a place to put it
             prev = iter;
             iter = iter->next;
         }
         
+        // If pizzaPerDollar are not the same then insert the new node right here
         prev->next = current;
         current->next = iter;
     }
@@ -120,24 +152,31 @@ int main(int argc, char* argv[]){
     // Open the file and make a pointer
     FILE* theFile = fopen(argv[1], "r");
     
+    // Allocate a pointer to the head pointer of linked list
     Pizza** head = (Pizza**)malloc(sizeof(Pizza*));
     *head = NULL;
     
 
     while (1){
+        
+        // Create a new node
         Pizza* current = create(theFile);
+        
+        // If the node returns null then either file is empty or finished
         if (current == NULL){
-            
             fclose(theFile);
             break;
         }
+        
+        // Insert the new node created to correct place in linked list
         insert(current, head);
-        //printf("%s\n", head->name);
     }
     
+    // Point an iterator to first node
     Pizza* iter = *head;
-    Pizza* temp;
+    Pizza* temp; // The third hand
     
+    // Print the list of pizza while releasing memory allocated
     while (iter != NULL){
         printf("%s %f\n", iter->name, iter->pizzaPerDollar);
         free(iter->name);
@@ -147,7 +186,6 @@ int main(int argc, char* argv[]){
     }
     
     // Release everything else
-
     free(head);
     return EXIT_SUCCESS;
 }
